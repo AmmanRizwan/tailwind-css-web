44f905b6b7b34b40de236a2e4035123ceb4198b7
AmmanRizwan <ammanrizwan00007@gmail.com>
2024-11-24 18:57:47 +0530

    improve the seo and change the logo and image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
   return (
     <footer className="w-screen bg-violet-300 py-4 text-black">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
-        <p className="text-center text-sm md:text-left">&copy; Nova 2024. All rights reserved</p>
+        <p className="text-center text-sm md:text-left font-circular-web">&copy; Amman 2024. All rights reserved</p>
 
         <div className="flex justify-center gap-4 md:justify-start">
           {links.map((link, index) => {
@@ -33,7 +33,7 @@ const Footer = () => {
             )
           })}
         </div>
-        <a href="#privacy-policy" className="text-center text-sm hover:underline md:text-right">Privacy Policy</a>
+        <a href="#privacy-policy" className="text-center text-sm hover:underline md:text-right font-circular-web">Privacy Policy</a>
       </div>
     </footer>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -60,7 +60,7 @@ const NavBar = () => {
       <header className="absolute top-1/2 w-full -translate-y-1/2">
       <nav className="flex size-full items-center justify-between p-4">
         <div className="flex items-center gap-7">
-          <img src="/img/logo.png" alt="logo" className="w-10" />
+          <img src="/img/logo.jpg" alt="logo" className="w-10 rounded-full" />
           <Button id='product-button' title="Products" rightIcon={<TiLocationArrow />} containerClass={'bg-blue-50 md:flex hidden items-center justify-center gap-1'} />
         </div>
         <div className="flex h-full items-center">
